5cd8b34e2bc98417046354b4db5b55cd6d00a3b9
Adrian <>
2024-12-10 10:06:06 +0100

    Bootstrap Nest project with Express, Axios, Jest and Supertest

---
+++ backend/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Allow localhost on any port for development purposes.
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://localhost:')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
+  await app.listen(3000);
+}
+
+bootstrap();
