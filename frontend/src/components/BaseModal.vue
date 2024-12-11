6887d5de32c2d467746bada528895e6db02e1da1
Adrian <>
2024-12-11 17:15:19 +0100

    Add base modal

---
+++ frontend/src/components/BaseModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    v-if="show"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+  >
+    <div
+      class="relative bg-white border border-slate-200 rounded-md shadow-lg w-[773px]"
+    >
+      <div class="px-8 py-4 border-b border-slate-200">
+        <h2 class="text-2xl font-medium text-slate-800">
+          <slot name="title"></slot>
+        </h2>
+      </div>
+
+      <div class="px-8 pb-4">
+        <slot name="body"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { provide } from "vue";
+
+defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:show"]);
+
+function close() {
+  emit("update:show", false);
+}
+
+provide("closeModal", close);
+</script>
